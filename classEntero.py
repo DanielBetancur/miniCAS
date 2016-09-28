3e297e5983afcc946b147740f4b686b5de2adcfc
Danielbetancur <Danielbetancur1997@gmail.com>
2016-09-28 15:14:30 -0500

    Agrego la primera parte de la resta en la clase

--- classEntero.py
+++ classEntero.py
@@ -2,8 +2,9 @@
 """
 Created on %(28/sept/2016)s
 
-@author: %(danielbetancur)s
+@author: %(danielbetancur   )s
 """
+import numpy as np 
 class Entero:
     def __init__(self,E):
         ent=[]
@@ -32,7 +33,7 @@ class Entero:
             string+=str(i)
         return string 
     def SumaE (self, other):
-        import numpy as np 
+        
 
     #alguien me ayuda a crear un programa que verifique si es 
         a=self.entero
@@ -75,6 +76,58 @@ class Entero:
             for i in Sum:
                 string+=str(i)
             return string 
-a=Entero("2343225")
-b=Entero("12123898")
-print a.SumaE(b)
+    def RestaE (self, other):
+        #alguien me ayuda a crear un programa que verifique si es 
+        a=self.entero
+        b=other.entero
+        if len(a)>len(b):
+            menor=len(b)
+            mayor=len(a)
+                
+            Sum=np.zeros(mayor)
+            for i in range(-1,-1*menor-1,-1):
+                Sum[i]=int(a[i])-int(b[i])+Sum[i]
+                if Sum[i]<0:
+                    Sum[i]=Sum[i]+10
+                    Sum[i-1]=Sum[i-1]-1
+            for i in range(0,mayor-menor,1):
+                if len(a)>len(b):
+                    Sum[i]=Sum[i]+int(a[i]) 
+        elif len(a)<len(b):
+            x=a
+            a=b
+            b=x
+            menor=len(b)
+            mayor=len(a)
+            Sum=np.zeros(mayor, dtype=int)
+            
+            for i in range(-1,-1*menor-1,-1):
+                Sum[i]=int(a[i])-int(b[i])+Sum[i]
+                if Sum[i]<0:
+                    Sum[i]=Sum[i]+10
+                    Sum[i-1]=Sum[i-1]-1
+            for i in range(0,mayor-menor,1):
+                if len(a)>len(b):
+                    Sum[i]=Sum[i]+int(a[i])
+            if Sum[0]==0:
+                Sum=delete(Sum,[0])
+                Sum[0]=Sum[0]*-1
+            else:
+                Sum[0]=Sum[0]*-1
+
+        else:
+                Sum=np.zeros(len(a)+1, dtype=int)
+                for i in range(-1*len(a),0,1): 
+                    Sum[i]=int(a[i])-int(b[i])-Sum[i]
+                    if Sum[i]<0:
+                        Sum[i+1]=Sum[i+1]-1
+                        Sum[i]=Sum[i]+ 1                    
+                string=""
+                for i in Sum:
+
+                    string+=str(i)
+                return string
+        string=""
+        for i in Sum:
+            string+=str(i)
+        return string   
